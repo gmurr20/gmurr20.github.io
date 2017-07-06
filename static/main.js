656bffc0abe67c0fbadc3e1b9f13f366fd2cb8f5
Greg Murray <Gmurr@Gregs-MacBook-Pro.local>
2017-07-05 23:27:38 -0500

    minor change

---
+++ static/main.js
@@ -0,0 +1,9 @@
+$("#navBar").click(function() {
+	var links = ['top', 'aboutMe', 'project', 'travel', 'contact'];
+	var scrollTop = $(window).scrollTop();
+	console.log(scrollTop);
+
+	// 0-620 top section
+	//620-1900
+	//1900-2700
+});
