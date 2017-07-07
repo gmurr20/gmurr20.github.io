894fa7709c3f9c5b6b83b571b9010331fb0675fd
Greg Murray <Gmurr@Gregs-MacBook-Pro.local>
2017-07-06 21:59:54 -0500

    small changes

--- static/main.js
+++ static/main.js
@@ -7,3 +7,11 @@ $("#navBar").click(function() {
 	//620-1900
 	//1900-2700
 });
+
+function set_body_height() { // set body height = window height
+    $("#topPic").height($(window).height());
+  }
+  $(document).ready(function() {
+    $(window).bind('resize', set_body_height);
+    set_body_height();
+  });
