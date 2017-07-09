c4a661cce2e5698935437ba8d3c0d3b368fe0c33
Greg Murray <Gmurr@Gregs-MacBook-Pro.local>
2017-07-09 12:27:29 -0500

    nav arrows

--- static/main.js
+++ static/main.js
@@ -1,14 +1,50 @@
-$("#navBar").click(function() {
-	var links = ['top', 'aboutMe', 'project', 'travel', 'contact'];
-	var scrollTop = $(window).scrollTop();
-	console.log(scrollTop);
-
-	// 0-620 top section
-	//620-1900
-	//1900-2700
+$("#upArrow").click(function() {
+	var scrollTop = $(window).scrollTop() - 50;
+	var top= 0;
+	var work = $("#work").offset().top;
+	var projects = $("#projects").offset().top;
+	var travel = $("#travel").offset().top;
+	var contact = $("#contact").offset().top;
+	if(scrollTop < work) {
+		scrollTo(top);
+	}
+	else if(scrollTop < projects) {
+		scrollTo(work);
+	}
+	else if(scrollTop < travel) {
+		scrollTo(projects);
+	}
+	else {
+		scrollTo(travel);
+	}
 });
 
-function set_body_height() { // set body height = window height
+$("#downArrow").click(function() {
+	var scrollTop = $(window).scrollTop() + 50;
+	var work = $("#work").offset().top;
+	var projects = $("#projects").offset().top;
+	var travel = $("#travel").offset().top;
+	var contact = $("#contact").offset().top;
+	if(scrollTop < work) {
+		scrollTo(work);
+	}
+	else if(scrollTop < projects) {
+		scrollTo(projects);
+	}
+	else if(scrollTop < travel) {
+		scrollTo(travel);
+	}
+	else {
+		scrollTo(contact);
+	}
+});
+function scrollTo(top) {
+    $('html, body').animate({
+        scrollTop: top
+	}, 1000);
+}
+
+function set_body_height() {
     $("#topPic").height($(window).height());
   }
   $(document).ready(function() {
